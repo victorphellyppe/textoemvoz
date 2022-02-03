79aa08eccd19a711b5dace93b31dbf7045d9ba45
Victor Oliveira <victorphellyppe@hotmail.com>
2022-02-03 09:41:41 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-texttospeech-example',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
